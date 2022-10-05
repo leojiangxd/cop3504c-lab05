b34f5e72f99b446bfc142482845d677e2f4fa5d8
leojiangxd <114763373+leojiangxd@users.noreply.github.com>
2022-10-05 16:04:24 -0400

    working

--- analyzer.cpp
+++ analyzer.cpp
@@ -1,8 +1,4 @@
-//
-// Created by Leo on 10/5/2022.
-//
-
-#include <StringData>
+#include "StringData.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,7 +6,7 @@
 
 int linear_search(std::vector<std::string> container, std::string element)
 {
-    for(i = 0; i<container.length(); i++)
+    for(int i = 0; i<container.size(); i++)
         if (container[i] == element)
             return i;
     return -1;
@@ -19,33 +15,63 @@ int linear_search(std::vector<std::string> container, std::string element)
 int binary_search(std::vector<std::string> container, std::string element)
 {
     int low = 0;
-    high = container.length() - 1;
-    while(high>=low)
+    int high = container.size() - 1;
+    int mid = int((low + high) / 2);
+    while (low != high)
     {
-        mid = int(high+low);
-        if (container[mid] < element)
-            low = mid + 1;
-        else if (container[mid] > element)
-            high = mid - 1;
-        else
+        if (container[mid] == element)
             return mid;
-        return -1;
+        else if (container[mid] < element)
+        {
+            low = mid + 1;
+            mid = int((low + high) / 2);
+        }
+        else
+        {
+            if (mid - 1 != 0)
+                high = mid - 1;
+            else
+                high = mid;
+            mid = int((low + high) / 2);
+        }
     }
+    return -1;
 }
 
 int main()
 {
-    std::vector<std::string> stringData = StringData.getStringData();
-    long currentTime = systemTimeNanoseconds();
-    std::string element;
-    cin >> element;
-    int index = linear_search(stringData, element);
-    currentTime = systemTimeNanoseconds() - currentTime;
-    cout << "linear " << element << "found in "<< currentTime << " seconds at index " << index
+    std::vector<std::string> stringData = getStringData();
+    long long time = systemTimeNanoseconds();
+    int index;
+    index = linear_search(stringData, "not_here");
+    time = systemTimeNanoseconds() - time;
+    std::cout << "Linear (not_here); Index " << index << " in " << time / (double)1000 << " seconds\n";
 
-    index = binary_search(stringData, element);
-    currentTime = systemTimeNanoseconds() - currentTime;
-    cout << "linear " << element << "found in "<< currentTime << " seconds at index " << index
+    systemTimeNanoseconds();
+    index = binary_search(stringData, "not_here");
+    time = systemTimeNanoseconds() - time;
+    std::cout << "Binary (not_here); Index " << index << " in " << time / (double)1000 << " seconds\n";
+
+    systemTimeNanoseconds();
+    index = linear_search(stringData, "mzzzz");
+    time = systemTimeNanoseconds() - time;
+    std::cout << "Linear (mzzzz); Index " << index << " in " << time / (double)1000 << " seconds\n";
+
+    systemTimeNanoseconds();
+    index = linear_search(stringData, "mzzzz");
+    time = systemTimeNanoseconds() - time;
+    std::cout << "Binary (mzzzz); Index " << index << " in " << time / (double)1000 << " seconds\n";
+
+    systemTimeNanoseconds();
+    index = linear_search(stringData, "aaaaa");
+    time = systemTimeNanoseconds() - time;
+    std::cout << "Linear (aaaaa); Index " << index << " in " << time / (double)1000 << " seconds\n";
+
+    systemTimeNanoseconds();
+    index = linear_search(stringData, "aaaaa");
+    time = systemTimeNanoseconds() - time;
+    std::cout << "Binary (aaaaa); Index " << index << " in " << time / (double)1000 << " seconds\n";
+
     return 0;
 }
 
